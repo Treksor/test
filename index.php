8fa90a6948d0e335dd8f9ac30611faaedf3ddc58
Igor Serykh <dexterm0r9an@gmail.com>
2018-09-11 12:00:10 +0700

    stormCommit

--- index.php
+++ index.php
@@ -23,116 +23,67 @@ discount = '. mt_rand(0,2).';
     
 ';
     $bd = parse_ini_string($ini_string,true);
-//    print_r($bd);
+    print_r($bd);
+//    $output=array(                  //массив на выход для заполнения
+//    'Наименование'=>0,
+//    'Заказано'=>0,
+//    'Остаток на складе'=>0,
+//    'Стоимость без скидки'=>0,
+//    'Стоимость со скидкой'=>0,
+//    );
 
-     $item1='Игрушка мягкая мишка белый';
-     $item2='Одежда детская куртка синяя синтепон';
-     $item3='Игрушка детская велосипед';
-
-     $price1=$bd['игрушка мягкая мишка белый']['цена'];
-     $price2=$bd['одежда детская куртка синияя синтепон']['цена'];
-     $price3=$bd['игрушка детская велосипед']['цена'];
-
-     $order1=$bd['игрушка мягкая мишка белый']['количество заказано'];
-     $order2=$bd['одежда детская куртка синияя синтепон']['количество заказано'];
-     $order3=$bd['игрушка детская велосипед']['количество заказано'];
-
-     $storage1=$bd['игрушка мягкая мишка белый']['остаток на складе'];
-     $storage2=$bd['одежда детская куртка синияя синтепон']['остаток на складе'];
-     $storage3=$bd['игрушка детская велосипед']['остаток на складе'];
-
-     $discount1=$bd['игрушка мягкая мишка белый']['discount'];
-     $discount2=$bd['одежда детская куртка синияя синтепон']['discount'];
-     $discount3=$bd['игрушка детская велосипед']['discount'];
-
-
-    function item($order,$storage,$price,$item='Игрушка мягкая мишка белый'){
-        global $fullprice,$count;
-        $result= "$item, в колличестве $order ед. товара";
-        if ($storage==0) {
-            $result.= ", но, к сожалению, данный товар закончился на складе";
-        }
-        elseif ($order>$storage){
-            $result.= ", но, к сожалению, у нас на складе только $storage ед. товара. ";
-        }
-        else {
-            $result.= ". Cтоимость 1ед. товара составляет $price$";
-            $fullprice+=($order*$price);
-            $count+=$order;
+    function order($order, $storage)                //проверка заказ <-> остаток
+    {
+        if ($storage == 0 || $order>$storage) {
+        $result = 0;
+        } else {
+        $result = 1;
         }
         return $result;
     }
 
 
-    function sale($order,$storage){
-        if ($order>=3 & $storage>$order){
-            $special='За покупку трех и более велосипедов вы получаете скидку 30%';
-        }
-        else {
-            $special='';
-        }
-        return $special;
-    }
-
-
-
-
-    function disc($item,$order,$price,$discount=''){
-            global $fullprice;
-            switch($discount){
-            case '1':{
-                $profit=(($order*$price)/100)*10;
-                $text= "скидка на $item стоставляет 10%, вы экэномите $profit$";
-                $fullprice-=$profit;
-                break;
+    function discount($disc,$price)             //вычисление цены со скидкой
+    {
+        switch ($disc){
+            case '1':
+            {
+            $price=$price*0.9;
+            break;
             }
-            case '2':{
-                $profit=(($order*$price)/100)*20;
-                $text= "скидка на $item стоставляет 20%, вы экэномите $profit$";
-                $fullprice-=$profit;
-                break;
+            case '2':
+            {
+            $price=$price*0.8;
+            break;
             }
-                case '3':{
-                 $profit=(($order*$price)/100)*30;
-                 $text= "скидка на $item стоставляет 30%, вы экэномите $profit$";
-                 $fullprice-=$profit;
-                }
-                break;
-            default:
-                $text= '';
-                break;
         }
-        return $text;
+        return $price;
     }
 
-     $count=0;
-     $disc='disc';
-     $fullprice=0;
-     echo 'Вы заказали:<br>';
-     echo item($order1,$storage1,$price1),'<br>';
-     echo item($order2,$storage2,$price2,$item2),'<br>';
-     echo item($order3,$storage3,$price3,$item3),'<br>';
+    foreach ($bd as $key=>$item){
 
-     echo "Итого вы заказали $count вещей на сумму $fullprice$",'<br>','<br>';
+         if (order($item['количество заказано'],$item['остаток на складе'])) {
+             $count = $item['количество заказано'];
+             $fullprice=$item['цена']*$item['количество заказано'];
+         }
+         else {
+             $count=$item['остаток на складе'];
+             $fullprice=$item['цена']*$item['остаток на складе'];
+         }
 
-    if ($count==0){
-        exit;
+         if ($key='игрушка детская велосипед'&$item['количество заказано']>=3){
+             $discount=$item['цена']*0.7;
+             $fullpricedisc=$discount*$count;
+         }
+         else{
+             $discount=discount($item['discount'],$item['цена']);
+             $fullpricedisc=$discount*$count;
+         }
+
+
+
+
     }
-
-     echo ($order1<$storage1)? "{$disc($item1,$order1,$price1,$discount1)}<br>":'';
-     echo ($order2<$storage2)? "{$disc($item2,$order2,$price2,$discount2)}<br>":'';
-     if (sale($order3,$storage3))  {
-         echo '<h2>',sale($order3,$storage3),'</h2>';
-         echo $disc($item3,$order3,$price3,3),'<br>';
-     }
-     else{
-         echo ($order3<$storage3)? "{$disc($item3,$order3,$price3,$discount3)}<br>":'';
-         echo '<br>';
-     }
-
-
-     echo "Итого вы заказали $count вещей. Со скидкой сумма заказа составляет $fullprice$",'<br>';
-
 
 
 
@@ -141,3 +92,6 @@ discount = '. mt_rand(0,2).';
 
 
 ?>
+
+
+
