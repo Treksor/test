ad1885ebf85abb63127adf06a4b6e5e8cb6634f1
Treksor <dexterm0r9an@gmail.com>
2019-01-15 13:20:12 +0700

    treksor

--- index.php
+++ index.php
@@ -52,11 +52,10 @@ if (array_key_exists('data',$_SESSION) & !empty($_SESSION['data'])){
     $allData=$_SESSION['data'];
 }
 
-print_r($_SESSION); echo '<br><br><br>';
-print_r($allData); echo '<br><br><br>';
-print_r($item);
-//if (isset($_GET['open'])){
-//    if (!empty($allData)){
+//print_r($_SESSION); echo '<br><br><br>';
+//print_r($allData); echo '<br><br><br>';
+//print_r($item);
+
 
 ?>
 
